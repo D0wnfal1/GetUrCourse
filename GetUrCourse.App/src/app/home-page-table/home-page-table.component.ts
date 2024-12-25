6d008392b41b6e22627b49a378ca40ae227927e7
D0wnfal1 <deadofgreaf@gmail.com>
2024-12-25 13:29:53 +0200

    Init App project n update solutionfile

---
+++ GetUrCourse.App/src/app/home-page-table/home-page-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page-table.component.html',
+  styleUrl: './home-page-table.component.css'
+})
+export class HomePageTableComponent {
+
+}

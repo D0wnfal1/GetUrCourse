44de263aa06bc4219194974fe38c068caf630df6
Dmytro Zanuda <dmzanuda.work@gmail.com>
2025-02-13 14:08:21 +0200

    update file structure in solution

---
+++ tests/User/GetUrCourse.User.UnitTests/UserControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentResults;
+using GetUrCourse.Services.UserAPI.Application.UseCases.Users.Commands.Create;
+using GetUrCourse.Services.UserAPI.Application.UseCases.Users.Commands.Update;
+using GetUrCourse.Services.UserAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace GetUrCourse.User.UnitTests;
+
+public class UserControllerTests
+{
+	private readonly UserController _controller;
+	private readonly Mock<ISender> _mockSender;
+
+	public UserControllerTests()
+	{
+		_mockSender = new Mock<ISender>();
+		_controller = new UserController(_mockSender.Object);
+	}
+	
+}
